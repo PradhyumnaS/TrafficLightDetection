f3d6649065366bd3699c39a2b2a139907b4e027c
Pradhyumna S <pradhyumnas@Pradhyumnas-MacBook-Air.local>
2024-11-17 00:36:05 +0530

    Cleanup

--- implementation/app.py
+++ implementation/app.py
@@ -7,15 +7,12 @@ import tempfile
 
 app = Flask(__name__)
 
-# Load the YOLO model
 model = YOLO('models/best_traffic_small_yolo.pt')
 
-# Home route for the HTML form
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Route to handle the image upload
 @app.route('/upload', methods=['POST'])
 def upload_image():
     if 'image' not in request.files:
@@ -24,27 +21,21 @@ def upload_image():
     if file.filename == '':
         return "No selected file", 400
     
-    # Create a temporary file to store the uploaded image
     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".jpg")
     file.save(temp_file.name)
 
-    # Process the uploaded image with YOLO model
     results = model.predict(source=temp_file.name)
 
-    # Get the first result (if there are multiple predictions)
     for r in results:
         im_array = r.plot()
-        im = Image.fromarray(im_array[..., ::-1])  # Convert BGR to RGB
+        im = Image.fromarray(im_array[..., ::-1])
 
-        # Save the result to a bytes buffer
         img_io = io.BytesIO()
         im.save(img_io, 'JPEG')
         img_io.seek(0)
 
-        # Clean up temporary file
         os.remove(temp_file.name)
 
-        # Send the processed image back to the user
         return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='results.jpg')
 
     return "Error with image processing", 500
